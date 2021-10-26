5d6cb10b19f6798df9d1a0bd53505fa7be5df97c
shalin <shalin.rono@student.moringaschool.com>
2021-10-26 22:40:20 +0300

    add email verification url

--- authentication/urls.py
+++ authentication/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import RegisterView
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name="register"),
+    path('register/', RegisterView.as_view(), name="register"
+    path('email-verify/', VerifyEmail.as_view(), name="email-verify"),
 ]
